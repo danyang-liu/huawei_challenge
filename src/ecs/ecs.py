3248b3e368c1ab23e65541858f63e85f53b99d37
Danyang Liu <ldy591@mail.ustc.edu.cn>
2018-04-04 22:00:20 +0800

    add a new line

--- src/ecs/ecs.py
+++ src/ecs/ecs.py
@@ -14,6 +14,7 @@ def main():
     ecsDataPath = sys.argv[1]
     inputFilePath = sys.argv[2]
     resultFilePath = sys.argv[3]
+
     # ecsDataPath = '../../data/TrainData_2015.1.1_2015.2.19.txt'
     # inputFilePath = '../../data/input_5flavors_cpu_7days.txt'
     # resultFilePath = '../../data/result.txt'
