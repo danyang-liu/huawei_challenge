dbd7880a3ed403a4e3ee86a54d61e63db5190e05
Danyang Liu <ldy591@mail.ustc.edu.cn>
2018-04-05 23:04:56 +0800

    权重调参1

--- src/ecs/predictor.py
+++ src/ecs/predictor.py
@@ -59,7 +59,11 @@ def predict_vm(ecs_lines, input_lines):
         predict_flavor_num.append(0)
         for item in esc_data:
             if item[1] == flavor_type[index]:
-                predict_flavor_num[index] = predict_flavor_num[index] + 2-float((predict_date_start-item[2]).days)/float(train_days_delta)
+                predict_flavor_num[index] = predict_flavor_num[index] + 2-2*float((predict_date_start-item[2]).days)/float(train_days_delta)
+                test_date = item[2]
+                test1 = float((predict_date_start - item[2]).days)
+                test = float((predict_date_start-item[2]).days)/float(train_days_delta)
+                pass
 
     for index in range(flavor_type_num):
         predict_flavor_num[index] = int((predict_flavor_num[index]*float((predict_date_end-predict_date_start).days+1))/train_days_delta)
